e1aabbcd2e9cef6093179522cf37135d04315df9
DieggoAn <dieggoan@outlook.com>
2023-11-11 12:47:17 -0300

    pruebas random

--- main.hs
+++ main.hs
@@ -1,6 +1,8 @@
 import System.Environment (getArgs)
 import Control.Monad (replicateM_)
 import System.Process
+import System.Random
+
 
 -- Function to print a single row
 printRow :: Int -> IO ()
@@ -9,6 +11,15 @@ printRow m = putStrLn $ unwords (replicate m "■")
 -- Function to print the matrix
 printMatrix :: Int -> IO ()
 printMatrix m = replicateM_ m (printRow m)
+
+-- Function to generate a random number between 1 and 5 with a seed
+randomNumberWithSeed :: Int -> IO ()
+randomNumberWithSeed seed = do
+    let generator = mkStdGen seed
+        randomNumber :: Int
+        randomNumber = fst $ randomR (1, 5) generator
+    putStrLn $ "Random number between 1 and 5 with seed " ++ show seed ++ ": " ++ show randomNumber
+
 
 main :: IO ()
 main = do
@@ -24,5 +35,5 @@ main = do
                 then do
                     _ <- system "clear"  -- Use "cls" on Windows
                     main
-                else putStrLn "Boa noite bld"
+                else randomNumberWithSeed 420
         _ -> putStrLn "Para ejecutar: ./Jogo <tamano>"
